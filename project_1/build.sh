d5bd42075ef2f58a5ecb2d6b2acb2639b91e123b
Harrison Cross <github@chimpjuice.com>
2018-04-11 23:32:30 -0400

    don't download gradle, move image maker to directory on its own

--- project_1/build.sh
+++ project_1/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm reddit/android:java8-r24-4-1  /bin/sh -c "./gradlew build"
+docker run --rm -it -v $(pwd):/opt/workspace --workdir=/opt/workspace --rm reddit/android:java8-r24-4-1  /bin/sh -c "./gradlew build"
